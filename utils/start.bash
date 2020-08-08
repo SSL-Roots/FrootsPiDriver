973bb985f3e3ed03b3d13209b3cf52b87386d084
ShotaAk <macakasit@gmail.com>
2020-08-08 13:24:46 +0000

    Fix script

--- utils/start.bash
+++ utils/start.bash
@@ -2,13 +2,13 @@
 
 dir=$(dirname $0)/../
 
-if [ -e "/sys/class/frootspi" ]; then
+if [ -e "/sys/class/frootspi_hello" ]; then
     sudo rmmod frootspi
 fi
 
 echo $dir
 cd $dir/src/drivers/
-make clean
+#make clean
 make
 sudo insmod frootspi.ko
 sleep 1
